ff8312c31c7b53930e8bb746dd3893a775acaf7b
Graskian <graskian@gmail.com>
2018-10-19 10:57:22 +0200

    Update edof_functions.R

--- edof_functions.R
+++ edof_functions.R
@@ -1,12 +1,16 @@
 library(rebird)
 
-ebaseFilter <- function(loc,species=NULL,back=30){
+ebaseFilter <- function(loc,species=NULL,back=30,ename=F){
   
   dat <- ebirdregion(loc=loc,species=species,back=back,provisional=T)
   if(dim(dat)[1]==0){
     dat <- NULL
   }else{
-    dat <- as.data.frame(dat[,c(6,2,7,5)])
+    if(ename==F){
+      dat <- data.frame(obsDt=dat$obsDt,comName=dat$speciesCode,howMany=dat$howMany,locName=dat$locName)
+    }else{
+      dat <- data.frame(obsDt=dat$obsDt,comName=dat$comName,howMany=dat$howMany,locName=dat$locName)
+    }  
   }
   
   return(dat)
@@ -17,11 +21,12 @@ ebaseFilterEU <- function(locs="europe",spc="all",days=30){
   
   dat <- data.frame(obsDT='N',comName='N',howMany=0,locName='N',country='N')[-1,]
   if(locs=="europe"){
-    locs <- c('DK','SE','NO','FR','DE','NL','GB')
+    locs <- c('DK','SE','NO','FR','DE','NL','GB','CH','LU','BE','ES','PT')
   }
   
   if(spc == "all"){
-    spc <- c("sooshe","manshe","lcspet","yebwar3","palwar5","duswar","refblu","radwar1")
+    spc <- c("sooshe","manshe","lcspet","yebwar3","palwar5","duswar",
+             "refblu","radwar1","bripet","corshe","greshe")
   }
   
   for(i in 1:length(locs)){
@@ -37,11 +42,32 @@ ebaseFilterEU <- function(locs="europe",spc="all",days=30){
     
   }
   
-  dat$obsDt <- as.Date(dat$obsDt)
-  dat$comName <- engtodan(dat$comName)
-  dat <- dat[order(dat$obsDt,decreasing=T),]
-  row.names(dat) <- NULL
+  if(dim(dat)[1]==0){
+    dat <- NULL
+  }else{
+    dat$obsDt <- as.Date(dat$obsDt)
+    dat$comName <- codetodan(dat$comName)
+    dat <- dat[order(dat$obsDt,decreasing=T),]
+    row.names(dat) <- NULL
+  }
+  
   return(dat)
+  
+}
+
+codetodan <- function(x){
+  
+  x <- gsub('yebwar3','Hvidbrynet Løvsanger',x)
+  x <- gsub('duswar','Brun Løvsanger',x)
+  x <- gsub('lcspet','Stor Stormsvale',x)
+  x <- gsub('manshe','Almindelig Skråpe',x)
+  x <- gsub('sooshe','Sodfarvet Skråpe',x)
+  x <- gsub('palwar5','Fuglekongesanger',x)
+  x <- gsub('refblu','Blåstjert',x)
+  x <- gsub('radwar1','Schwarz Løvsanger',x)
+  x <- gsub('bripet','Lille Stormsvale',x)
+  x <- gsub('corshe','Kuhls Skråpe',x)
+  x <- gsub('greshe','Storskråpe',x)
   
 }
 
