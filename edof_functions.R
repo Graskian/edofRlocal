dd5a8e472f4c7e557e1235d2cffca2925ce14102
matbbDTU <graskian@gmail.com>
2019-03-13 11:16:37 +0800

    Update edof_functions.R

--- edof_functions.R
+++ edof_functions.R
@@ -1,3 +1,5 @@
+# Personal eBird data extraction library Version 1.01
+
 library(rebird)
 
 ebaseFilter <- function(loc,species=NULL,back=30,ename=F){
@@ -20,13 +22,14 @@ ebaseFilter <- function(loc,species=NULL,back=30,ename=F){
 ebaseFilterEU <- function(locs="europe",spc="all",days=30){
   
   dat <- data.frame(obsDT='N',comName='N',howMany=0,locName='N',country='N')[-1,]
-  if(locs=="europe"){
+  if(locs[1]=="europe"){
     locs <- c('DK','SE','NO','FR','DE','NL','GB','CH','LU','BE','ES','PT')
   }
   
   if(spc == "all"){
     spc <- c("sooshe","manshe","lcspet","yebwar3","palwar5","duswar",
-             "refblu","radwar1","bripet","corshe","greshe")
+             "refblu","radwar1","bripet","corshe","greshe","feapet1",
+             "reevir1")
   }
   
   for(i in 1:length(locs)){
@@ -55,6 +58,27 @@ ebaseFilterEU <- function(locs="europe",spc="all",days=30){
   
 }
 
+DKUfilter <- function(x){
+  
+  rem <- numeric(0)
+  rem <- c(rem,which(is.na(x$howMany)))
+  rem <- c(rem,which(x$comName=="Kuhls Skråpe" & x$country=="ES" & x$howMany < 300))
+  rem <- c(rem,which(x$comName=="Almindelig Skråpe" & x$country=="ES" & x$howMany < 2))
+  rem <- c(rem,which(x$comName=="Almindelig Skråpe" & x$country=="GB" & x$howMany < 3))
+  rem <- c(rem,which(x$comName=="Hvidbrynet Løvsanger" & x$country=="ES" & x$howMany < 2))
+  rem <- c(rem,which(x$comName=="Hvidbrynet Løvsanger" & x$country=="GB" & x$howMany < 2))
+  rem <- c(rem,which(x$comName=="Hvidbrynet Løvsanger" & x$country=="FR" & x$howMany < 2))
+  rem <- c(rem,which(x$comName=="Hvidbrynet Løvsanger" & x$country=="PT" & x$howMany < 2))
+  rem <- c(rem,which(x$comName=="Hvidbrynet Løvsanger" & x$country=="NL" & x$howMany < 2))
+  rem <- c(rem,which(x$comName=="Hvidbrynet Løvsanger" & x$country=="SE" & x$howMany < 2))
+  rem <- c(rem,which(x$comName=="Hvidbrynet Løvsanger" & x$country=="DE" & x$howMany < 2))
+  
+  x <- x[-rem,]
+  
+  return(x)
+  
+}
+
 codetodan <- function(x){
   
   x <- gsub('yebwar3','Hvidbrynet Løvsanger',x)
@@ -68,6 +92,9 @@ codetodan <- function(x){
   x <- gsub('bripet','Lille Stormsvale',x)
   x <- gsub('corshe','Kuhls Skråpe',x)
   x <- gsub('greshe','Storskråpe',x)
+  x <- gsub('feapet1',"Kap-Verde Petrel",x)
+  x <- gsub('brnboo',"Brun Sule",x)
+  x <- gsub('reevir1',"Rødøjet Vireo",x)
   
 }
 
